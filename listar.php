f9389b968c5310d4662167a2f9dd1f7c2cd3c748
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-08-07 17:19:17 -0500

    Update listar.php

--- listar.php
+++ listar.php
@@ -1,26 +1,24 @@
 <?php
-$db_host = "rmspavs8mpub7dkq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-     $db_name = "n3ycfw3efefiynuy";
-     $db_user = "g1wbv4e6yvi6e710";
-     $db_password = "t2z30tum76qfgrzd";
-    
-     $connection = mysql_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysql_error());
-    
-mysql_select_db($db_name) or die("Error al seleccionar la base de datos:".mysql_error());
-    @mysql_query("SET NAMES 'utf8'");
+define('HOST','rmspavs8mpub7dkq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+      	define('USER','g1wbv4e6yvi6e710');
+      	define('PASS','t2z30tum76qfgrzd');
+		define('DB','n3ycfw3efefiynuy');
+	 $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+	
+
 if(isset($_POST["numero_identif"])){
 
 $numero_identif = $_POST["numero_identif"];
 $sql_query = "SELECT * FROM servicio where numero_identif=$numero_identif;";
-$result = mysql_query($sql_query);
+$result = mysqli_query($con,$sql_query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
 print json_encode($rows);
 }else
 {
 	echo "No existe el supe";
-mysql_close($connection);
+mysqli_close($con);
 }
 ?>
